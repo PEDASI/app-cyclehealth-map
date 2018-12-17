53f300eb7e89e5ee3a243d2da03ef29dc86af648
Steve Crouch <s.crouch@software.ac.uk>
2018-12-17 12:53:59 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# PEDASI Cycle Route Health Example
+
+## Introduction
+
+This PEDASI example shows how a JavaScript application can make use of the following to build and draw
+a cycling route map with a summary report of the health of that route:
+
+* Two PEDASI external data sources:
+  * BT CityVerve Portal: containing a fixed set of Manchester cycle path routes, with specific waypoint
+data obtainable for each route.
+  * CleanSpace: provides air quality data for a requested geolocation.
+* Leaflet.js: a JavaScript visualisation library used to plot the IoT businesses for a location on a
+zoomed map, using OpenStreetMap as the map plotting service.
+* JQuery: a JavaScript library used to simplify HTML DOM tree traversal and manipulation, and handle
+asynchronous Ajax calls to the PEDASI Applications API.
+* Bootstrap 4: an open-source front-end framework for designing web applications.
+
+## Prerequisites
+
+The following are required to run the application:
+
+* A web browser (e.g. Google Chrome, Mozilla Firefox, Apple Safari, Microsoft Edge but not Microsoft
+Internet Explorer).
+* Either:
+  * Access to the application hosted on GitHub gh-pages.
+  * A local web server (e.g. for simple testing, you can use Python's internal test web server).
+* A PEDASI Application or User API key, to authenticate with PEDASI to use its datasources.
+
+## Running the Example Locally (Optional)
+
+After cloning the repository, you can just do the following to host the application using Python 3's
+basic built-in web server, e.g. on Linux/Mac OS:
+
+```
+$ python3 -m http.server
+```
+
+By default, this will run the web server on port 8000.
+
+## Using the Application
+
+Then using a web browser:
+
+* If hosting the application locally on port 8000, go to http://localhost:8000.
+* If accessing the application via GitHub's gh-pages, go to https://southampton-rsg.github.io/app-cyclehealth-map/.
+
+You can then add your PEDASI Application or User API key, and select 'Get Routes'. The 'Cycle Route'
+dropdown will then be populated with all cycle routes within BT's cycle path catalogue. Click submit
+to get a breakdown on the map of the cycle route, along with air quality data for each route waypoint
+obtained from CleanSpace. Hovering over a waypoint will show the extent of pollutants for that waypoint
+in a popup.
+
+## License
---
+++ static/js/app.js
@@ -0,0 +1,321 @@
+/*jshint esversion: 6 */
+/*jshint esversion: 6 */
+
+"use strict";
+
+// PEDASI convenience set up
+const PEDASI_API = 'https://dev.iotobservatory.io';
+const DATASET_CYCLEROUTE_HC = '/api/datasources/4/data/';
+const DATASET_CYCLEROUTE_EN = '/api/datasources/3/datasets/';
+const DATASET_CLEANSPACE = '/api/datasources/5/data/';
+
+// Default client_id and key: for testing only!
+const CS_AUTH = "?clientId=CLIENT_ID&key=YOUR_KEY";
+
+const AQI_MAX = 60.0
+
+
+let map;
+
+/**
+ * Initialise a new Leaflet.js map focused on the UK.
+ * @returns {Map} map The leaflet map instance.
+ */
+function initialise_map() {
+    // Create our map and set the initial view to the UK
+    let mapOptions = {center: [17.385044, 78.486671], zoom: 10};
+    let map = new L.map('map', mapOptions);
+    let uk_bbox = [[49.82380908513249, -10.8544921875], [59.478568831926395, 2.021484375]];
+    map.fitBounds(uk_bbox);
+
+    // Add new TileLayer specifying the interface format to OpenStreetMap
+    let layer = new L.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+    map.addLayer(layer);
+
+    return map;
+}
+
+/**
+ * Return a colour based on value of air quality index, from green (good) to crimson (bad).
+ * @param {Number} aqi Value from 0-AQI_MAX indicating route average air quality
+ * @return {String} color_name Name of the colour assigned based on given index
+ */
+function get_color_from_aqi(aqi) {
+    let color_name = 'black';
+    switch (true) {
+        case (aqi < 5):
+            color_name = 'green';
+            break;
+        case (aqi < 10):
+            color_name = 'yellow';
+            break;
+        case (aqi < 15):
+            color_name = 'orange';
+            break;
+        case (aqi < 20):
+            color_name = 'red';
+            break;
+        case (aqi < 30):
+            color_name = 'purple';
+            break;
+        default:
+            color_name = 'crimson';
+            break;
+    }
+
+    return color_name;
+}
+
+/**
+ * Create a progressbar.js widget set to given air quality index number
+ * @param {Number} avg_aqi Value from 0-AQI_MAX indicating route average air quality
+ */
+function create_avg_aqi_index(avg_aqi) {
+    $("#aqi-circular").empty();
+
+    let bar_color = get_color_from_aqi(avg_aqi);
+    let aqi_widget = new ProgressBar.Circle('#aqi-circular', {
+        easing: 'easeInOut',
+        duration: 2000,
+        strokeWidth: 8,
+        trailWidth: 2,
+        from: {color: '#AA0000'},
+        to: {color: '#00AA00'},
+        color: bar_color,
+        text: {
+            style: {
+                color: '#000000',
+                position: 'relative',
+                top: '30%',
+                transform: {
+                    prefix: true,
+                    value: 'translate(0%, -192%)'
+                },
+                'text-align': 'center',
+                'font-size': '28px',
+                'font-weight': 'bold'
+            }
+        }
+    });
+
+    // Set widget text to value with 2 significant digits
+    aqi_widget.setText(Math.round(avg_aqi * 10) / 10);
+
+    // Set and animate the circular bar as a proportion of index magnitude and maximum value
+    aqi_widget.animate((AQI_MAX - Math.min(avg_aqi, AQI_MAX)) / AQI_MAX);
+}
+
+/**
+ * Add per-waypoint air quality data to route overview table.
+ * @param {Array} leaflet_wps Array of Leaflet lat/lng waypoints and CleanSpace air quality data
+ */
+function add_aqi_route_overview(leaflet_wps) {
+    // Add rows of air quality data to our route health overview table, with
+    // consecutive duplicate rows collapsed into single rows for brevity
+    let last_wp = null;
+    let last_wp_info = null;
+    for(let wp_num = 0; wp_num < leaflet_wps.length; wp_num++) {
+        let wp = leaflet_wps[wp_num];
+
+        // Extract the air quality data into an HTML-ised string
+        let cs_level = wp[1].index + ' ' + wp[1].level;
+        let cs_majpol = wp[1].major_pollutant.pollutant + '<br>' + wp[1].major_pollutant.description;
+        let cs_recs = wp[1].recommendations;
+        let new_wp_info = '<td>' + cs_level + '</td><td>' + cs_majpol + '</td><td>' + cs_recs + '</td></tr>';
+
+        if (wp_num === 0) {
+            // This is the first run through, so set the last known air quality data to this waypoint
+            last_wp = wp_num;
+            last_wp_info = new_wp_info;
+        } else if (new_wp_info !== last_wp_info || wp_num === leaflet_wps.length-1) {
+            // When we encounter a waypoint with different air quality data, add the previous
+            // captured data to our table
+            // Also add it if this is the last row in the waypoint array, otherwise it won't be added
+            let wp_ref = (wp_num === leaflet_wps.length-1) ? wp_num +1: wp_num;
+            let new_index = null;
+            if (wp_num == (last_wp + 1)) {
+                // There is only one row that contains this data, don't add a range
+                new_index = '<tr><td>' + (wp_ref) + '</td>';
+            } else {
+                // There are more than one row with the same data, add it as a range
+                new_index = '<tr><td>' + (last_wp+1) + '-' + (wp_ref) + '</td>';
+            }
+            $('#waypoint-data-table').append(new_index + last_wp_info);
+
+            // Record this waypoint air quality data so we can compare with others later
+            last_wp = wp_num;
+            last_wp_info = new_wp_info;
+        }
+    }
+}
+
+/**
+ * Add complete route to our map, with color of line between waypoints indicating overall air quality.
+ * @param {Array} leaflet_wps Array of Leaflet lat/lng waypoints and CleanSpace air quality data
+ * @param {FeatureGroup} l_group The Leaflet feature group to which we add our markers
+ */
+function add_route_to_map(leaflet_wps, l_group) {
+    // Populate the Leaflet feature group with colour-coded route polylines between each waypoint
+    for(let wp_num = 1; wp_num < leaflet_wps.length; wp_num++) {
+        let wp = leaflet_wps[wp_num];
+        let wp_p = leaflet_wps[wp_num-1];
+
+        let cs_aqi = wp[1].index;
+        let l_color = get_color_from_aqi(cs_aqi);
+        let l_polyline = L.polyline([wp_p[0], wp[0]], { color: l_color, weight: 2, dashArray: '5,4', lineJoin: 'round' });
+
+        // Add polyline to a feature group, to make it easier to find boundary of interest
+        l_group.addLayer(l_polyline);
+    }
+}
+
+/**
+ * Add waypoint markers to the map, each with popup info boxes of pollutants at that waypoint.
+ * @param {Array} leaflet_wps Array of Leaflet lat/lng waypoints and CleanSpace air quality data
+ * @param {FeatureGroup} l_group The Leaflet feature group to which we add our markers
+ */
+function add_waypoint_popups(leaflet_wps, l_group) {
+    // Add green start and red end markers for the route to our feature group
+    // Do this first so that they don't occlude the hover popups for these (added next),
+    // and will react to mouse events to show the popups
+    l_group.addLayer(L.circle(leaflet_wps[0][0], {color: 'green', weight: 1, fill: 'false', fillOpacity: 0.75, radius: 20}));
+    l_group.addLayer(L.circle(leaflet_wps[leaflet_wps.length - 1][0], {color: 'red', fillColor: 'red', fillOpacity: 0.75, radius: 20}));
+
+    // Add detailed air quality data as mouse hover popups for each waypoint to our feature group
+    for(let wp_num = 0; wp_num < leaflet_wps.length; wp_num++) {
+        let wp = leaflet_wps[wp_num];
+
+        // Add a new waypoint circular marker which pops up an info box with waypoint pollutant data
+        // when mouse hovers over it
+        let l_circle = L.circle(wp[0], {color: 'black', weight: 2, opacity: 0.5, fillOpacity: 0.0, fill: 'false', radius: 15}); //, popupAnchor: [200, -20]});
+        let aq_str = '<p><b>Waypoint ' + (wp_num+1) + '</b></p><p>';
+        for(let pol in wp[1].pollutant) {
+            aq_str += '<b>' + pol + ':</b> ' + wp[1].pollutant[pol] + '<br>';
+        }
+        aq_str += '</p>';
+        l_circle.bindTooltip(aq_str, {offset: [20, 0], sticky: true, direction: 'right'});
+        l_group.addLayer(l_circle);
+    }
+}
+
+/**
+ * Generate an air quality report for a given route.
+ * @param {Array} results Array of results from BT's cycle route data
+ * @param {Map} map The leaflet map instance
+ * @param {String} map The API key to authenticate with PEDASI
+ */
+async function generate_route_report(waypoints, map, pedasi_app_api_key) {
+    function sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    // Delete contents of waypoint overview data from any previous runs
+    $("#waypoint-data-table > tr").remove();
+
+    // Ask CleanSpace for air quality data for each waypoints, adding it to an array
+    let promises = [];
+    let leaflet_wps = [];
+    for(let wp_num = 0; (wp_num < waypoints.length) && (wp_num < 30); wp_num++) {
+        let waypoint = waypoints[wp_num];
+
+        let cleanspace_query_url = PEDASI_API + DATASET_CLEANSPACE + CS_AUTH + "&lat=" + waypoint[1] + "&lng=" + waypoint[0];
+        promises.push($.ajax({
+            url: cleanspace_query_url,
+            type: "GET",
+            headers: { 'Authorization': 'Token ' + pedasi_app_api_key },
+            success: function(response) {
+                leaflet_wps.push([L.latLng(waypoint[1], waypoint[0]), response.aqi]);
+            }, error: function(jq_xhr, exception) {
+                console.log("Error: " + jq_xhr, exception);
+            }
+        }));
+
+        // Reduce rate of CleanSpace API requests
+        await sleep(200);
+    }
+
+    // Convenience function to ignore raised errors from promises, since we only care
+    // about valid results and by default Promises.all() will not complete with >0 errors
+    function ignore(promise) {
+        return promise.catch(e => undefined);
+    }
+
+    // Wait until all our promise requests are finished (either failed or successful)
+    await Promise.all(promises.map(ignore));
+
+    // Calculate average air quality index for route
+    let avg_aqi = leaflet_wps.reduce(function (sum, wp) {
+        return sum + parseFloat(wp[1].index);
+    }, 0) / leaflet_wps.length;
+
+    // Create/replace average air quality index widget with calculated average
+    create_avg_aqi_index(avg_aqi);
+
+    // Create a Leaflet feature group on our map which contains the route and air quality markers,
+    // then add the route and the waypoint markers to it
+    let l_group = L.featureGroup().addTo(map);
+    add_route_to_map(leaflet_wps, l_group);
+    add_waypoint_popups(leaflet_wps, l_group);
+
+    // Add summary route report
+    add_aqi_route_overview(leaflet_wps);
+
+    // Determine the boundary for our polyline and markers, and restrict map view accordingly
+    map.fitBounds(l_group.getBounds());
+
+    // Make average air quality index and summary route report visible
+    $('#waypoint-report').removeClass('d-none');
+}
+
+/**
+ * Update map and summary with selected route and air quality data for that route.
+ * @returns {boolean} false Return false to ensure the page doesn't refresh on form submit.
+ */
+function populate_map() {
+    let cycle_route = $('#mapParamsCycleRoute').val();
+    let pedasi_app_api_key = $('#mapParamsAppKey').val();
+
+    // Request detailed BT route data for the selected route from PEDASI, using the provided
+    // PEDASI Application or User API key required to authorise the request, then generate
+    // air quality data for the route and add it all to the page
+    let dataset_url = PEDASI_API + DATASET_CYCLEROUTE_EN;
+    let ped_query_url = dataset_url + cycle_route + '/data';
+
+    $.ajax({
+        url: ped_query_url,
+        type: "GET",
+        headers: { 'Authorization': 'Token ' + pedasi_app_api_key },
+        success: function(response) {
+            let waypoints = response[0].loc.coordinates;
+            generate_route_report(waypoints, map, pedasi_app_api_key);
+        }
+    });
+
+    return false;
+}
+
+function get_cycle_routes() {
+    let pedasi_app_api_key = $('#mapParamsAppKey').val();
+
+    // Request all BT cycle routes from PEDASI, using the provided PEDASI Application or User
+    // API key required to authorise the request, then populate the cycle route dropdown
+    let ped_query_url = PEDASI_API + DATASET_CYCLEROUTE_HC;
+    $.ajax({
+        url: ped_query_url,
+        type: "GET",
+        headers: { 'Authorization': 'Token ' + pedasi_app_api_key },
+        success: function(response) {
+            for(let item_num in response.items) {
+                let route_href = response.items[item_num].href;
+                let route_name = response.items[item_num]['item-metadata'][0].val;
+                $('#mapParamsCycleRoute').append($('<option></option>').val(route_href).html(route_name));
+            }
+        }
+    });
+
+    return false;
+}
+
+function form_init() {
+    map = initialise_map();
+}
